28d3030046e3784f7f3dfb3e84a4440fab60e7a1
yellowplace <yellowplace@naver.com>
2023-12-05 22:46:40 +0900

    asdasd

--- backend/src/main/java/com/service/com/YorokoromBackendApplication.java
+++ backend/src/main/java/com/service/com/YorokoromBackendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheerupBackendApplication {
+public class YorokoromBackendApplication {
  
 	public static void main(String[] args) {
- 		SpringApplication.run(CheerupBackendApplication.class, args);
+ 		SpringApplication.run(YorokoromBackendApplication.class, args);
 	}
 }
