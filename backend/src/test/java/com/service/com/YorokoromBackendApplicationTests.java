d853426dae9478b37ea7e55fdfaa0144acac1fe7
yellowplace <yellowplace@naver.com>
2023-12-05 22:41:46 +0900

    asd

--- backend/src/test/java/com/service/com/YorokoromBackendApplicationTests.java
+++ backend/src/test/java/com/service/com/YorokoromBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CheerupBackendApplicationTests {
+class YorokoromBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,19 +1,35 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback,useRef } from "react";
 import axios from "axios";
-import Layout from "../layout/Layout";
-const Login = () => {
-  const [username,setUsername] = useState("");
+ const Login = () => {
+  const [userId,setUserId] = useState("");
   const [password,setPassword] = useState("");
-  
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const handleLogin =()=>{
+    axios
+    .get(`http://localhost:8080/api/loginAction`,{
+      userId : userId,
+      password : password
+  }
+    )
+    .then((response) => {
+    
+    })
+    .catch((error) => console.log(error));
+  };
+
   return (
    <div className="Login">
-    <div class="">
-        <input type="text" name="username" className="inputText" onChange={(e)=>setUsername(e.target.value)}></input>
-        <input type="password" name="password" className="inputText" onChange={(e)=>setPassword(e.target.value)}></input>
-
-
-    </div>
-
+      <div class="">
+        <form id="">
+            <label>아이디:</label>
+            <input type="text" name="username" className="inputText" onChange={(e)=>setUserId(e.target.value)}></input>
+            <label>비밀번호:</label>
+            <input type="password" name="password" className="inputText" onChange={(e)=>setPassword(e.target.value)}></input>
+        </form>
+      </div>
+    <button onClick={handleLogin}>Login</button>
 
    </div>
    )
